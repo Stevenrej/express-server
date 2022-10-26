56e1157ead7c2802996a57bb4e7c4da72736dc8a
Stevenrej <steveo732@gmail.com>
2022-10-25 20:28:53 -0500

    mvp

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const { application, request, response } = require('express');
 // ***** REQUIRES ******
 const express = require('express');
 require('dotenv').config();
-let data = require('./.data/pets.json');
+let data = require('./data/weather.json');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
@@ -19,31 +22,35 @@ app.get('/', (request, response) => {
   response.status(200).send('Welcome to the server!');
 });
 
-app.get('/hello', (request, response) => {
-  console.log(request.query);
-  let firstName = request.query.firstName;
-  let lastName = request.query.lastName;
-  response.status(200).send(`Howdy ${firstName} ${lastName} thanks for joining my server!`);
-});
 
-app.get('/pet', (request, response, next) => {
+app.get('/weather', (request, response, next) => {
   try {
-    let species = request.query.species;
-    console.log(species);
-    let dataToGroom = data.find(pet => pet.species === species);
-    let datasToSend = new Pet(dataToGroom)
-    response.status(200).send(datasToSend);
+    let city_name = request.query.city_name.toLowerCase();
+    let lat = Math.floor(request.query.lat);
+    let lon = Math.floor(request.query.lon);
+    let dataToGroom = data.find(weather => weather.city_name.toLowerCase() === city_name && Math.floor(weather.lon) === lon && Math.floor(weather.lat) === lat);
+
+    let arrData = dataToGroom.data.map((element) => {
+      return new Forecast(element);
+    });
+    response.status(200).send(arrData);
   } catch (error) {
     next(error);
   }
 });
 
-// class Pet {
-//   constructor(petObj){
-//     this.name = petObj.name,
-//     this.breed = petObj.breed,
-//   });
-// }
+class Forecast {
+  constructor(weatherObj) {
+    this.date = weatherObj.valid_date;
+    this.low = weatherObj.low_temp;
+    this.high = weatherObj.high_temp;
+    this.description = weatherObj.weather.description;
+    console.log(this.low);
+  }
+}
+
+
+
 
 app.get('*', (request, response) => {
   response.status(404).send('This route does not exist!');
