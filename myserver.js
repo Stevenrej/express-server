9e11959dc99f191eb207d2a160b90106bce60360
Stevenrej <steveo732@gmail.com>
2022-10-29 15:18:46 -0500

    lab10

---
+++ myserver.js
@@ -0,0 +1,52 @@
+// 'use strict';
+
+
+// console.log('hi!');
+
+// const { application, request, response } = require('express');
+// // ***** REQUIRES ******
+
+// const express = require('express');
+// require('dotenv').config();
+// const cors = require('cors');
+
+// const app = express();
+// const getWeather = require ('./modules/myweather.js');
+// const getMovie = require ('./modules/movies.js');
+
+// app.use(cors());
+
+// const PORT = process.env.PORT || 3002;
+
+// // *** END POINTS *****
+
+// app.get('/', (request, response) => {
+//   console.log('Terminal');
+//   response.status(200).send('Welcome to the server!');
+// });
+
+// app.get('/weather', getWeather);
+
+
+
+// app.get('/movie', getMovie);
+
+
+
+
+
+
+// app.get('*', (request, response) => {
+//   response.status(404).send('This route does not exist!');
+// });
+
+
+// //  ***** ERROR HANDLER *****
+// app.use((error, request, response, next) => {
+//   response.status(500).send(error.message);
+// });
+
+// // *** SERVER START *****
+
+// app.listen(PORT, () => console.log(`we are up and running on port ${PORT}`));
+
--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 'use strict';
 
-
-console.log('hi!');
-
-const { application, request, response } = require('express');
-// ***** REQUIRES ******
-
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
 
-const app = express();
-const getWeather = require ('./modules/weather.js');
+const getWeather = require('./modules/weather.js');
 const getMovie = require ('./modules/movies.js');
+const app = express();
 
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
-// *** END POINTS *****
 
 app.get('/', (request, response) => {
   console.log('Terminal');
   response.status(200).send('Welcome to the server!');
 });
 
-app.get('/weather', getWeather);
-
-
+app.get('/weather', weatherHandler);
 
 app.get('/movie', getMovie);
 
-
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  getWeather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Sorry. Something went wrong!');
+    });
+}
 
 
 
@@ -40,13 +39,4 @@ app.get('*', (request, response) => {
   response.status(404).send('This route does not exist!');
 });
 
-
-//  ***** ERROR HANDLER *****
-app.use((error, request, response, next) => {
-  response.status(500).send(error.message);
-});
-
-// *** SERVER START *****
-
-app.listen(PORT, () => console.log(`we are up and running on port ${PORT}`));
-
+app.listen(PORT, () => console.log(`Server up on ${process.env.PORT}`));
