a2d3e60e4ae9345d8ed8b9aef4aa69ede6e3c056
Stevenrej <steveo732@gmail.com>
2022-10-28 20:53:08 -0500

    movie cache

--- modules/movies.js
+++ modules/movies.js
@@ -1,28 +1,51 @@
 'use strict';
 const axios = require('axios');
 
-async function getMovie(request, response, next) {
+let cache = {};
 
-  let cityName = request.query.city_name;
+async function getMovie(request, response, next) {
 
 
 
   try {
-    let apiMovUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}&language=en-US&page=1&include_adult=false`;
+    let cityName = request.query.city_name;
+
+    //  KEY CREATION
+
+    let key = cityName + 'photo';
+
+    // IF Data exist and is in a valid timefram send data
+
+    if (cache[key] && (Date.now() - cache[key].timestamp < 1000 * 20 * 60)) {
+      console.log('cache hit, image present');
+      response.status(200).send(cache[key].data);
+    } else {
+      console.log('cache missed');
 
 
 
-    let apiDataM = await axios.get(apiMovUrl);
+      let apiMovUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}&language=en-US&page=1&include_adult=false`;
 
-    console.log(apiDataM.data.results);
-    let arrData = apiDataM.data.results.map((element) => {
-      return new Movie(element);
-    });
-    response.status(200).send(arrData);
+
+
+      let apiDataM = await axios.get(apiMovUrl);
+
+
+      let arrData = apiDataM.data.results.map((element) => {
+        return new Movie(element);
+
+      });
+      cache[key] = {
+        data: arrData,
+        timestamp: Date.now(),
+      };
+      response.status(200).send(arrData);
+    }
   } catch (error) {
     next(error);
   }
 }
+
 
 class Movie {
   constructor(movieObj) {
